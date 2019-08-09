dd3034c50f164c174a7d4c139c0fa3e0b1faf75d
Tarik Graoui <tarik.graoui@gmail.com>
2019-08-09 11:23:42 +0200

    cocktail controller and views completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+  Cocktail.create(
+    name: Faker::Games::Pokemon.move
+    # description: Faker::Food.description,
+    # ingredient: Faker::Food.ingredient
+  )
+end
